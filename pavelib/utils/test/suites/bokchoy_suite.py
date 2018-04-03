1c86833ba19199161a352f900481f1cd1ceb8d6d
Michael Youngstrom <myoungstrom@edx.org>
2018-04-03 15:30:59 -0400

    Revert temporary cache change

--- pavelib/utils/test/suites/bokchoy_suite.py
+++ pavelib/utils/test/suites/bokchoy_suite.py
@@ -140,8 +140,10 @@ def reset_test_database():
     defined in pavelib/database.py
     If not, reset the test database and apply migrations
     """
-    print(os.environ.get('USER', None))
-    update_local_bokchoy_db_from_s3()  # pylint: disable=no-value-for-parameter
+    if os.environ.get('USER', None) == 'jenkins':
+        update_local_bokchoy_db_from_s3()  # pylint: disable=no-value-for-parameter
+    else:
+        sh("{}/scripts/reset-test-db.sh --migrations".format(Env.REPO_ROOT))
 
 
 @task
